0ef441211e6a459c245d68daf7167c9dba623234
yash <chandaneyash10@gmail.com>
2024-12-26 16:30:23 +0530

    yeshu

--- server.py
+++ server.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def root():
-    return "My Python flask app v1.0"
+    return "Mera yeshu yeshu..... Bolna lagiiiii"
 
 app.run(host="0.0.0.0", port=4000)
